d79a99cd1f0f4867ab33daa81818ca3fe1a8eca1
nikesh-trecarten <ntrecarten@gmail.com>
2025-04-01 09:11:41 +0200

    added google map

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 function App() {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) {
+    throw new Error("Google Maps API key is not defined in .env");
+  }
+
+  const mapContainerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  const center = {
+    lat: 50.732791,
+    lng: 7.090617,
+  };
+
   return (
     <>
-      <h1>ZUSCH</h1>
+      <h1>Zusch!</h1>
+      <div className="map">
+        <LoadScript googleMapsApiKey={apiKey}>
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={center}
+            zoom={16}
+          ></GoogleMap>
+        </LoadScript>
+      </div>
     </>
   );
 }
