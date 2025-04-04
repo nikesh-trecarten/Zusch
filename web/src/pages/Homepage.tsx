5ccf22136e604a88f09f994aa2efe36c64baafdd
nikesh-trecarten <ntrecarten@gmail.com>
2025-04-04 09:58:42 +0200

    added clerk for signin/register/signout, welcome message to homepage specifies username

--- web/src/pages/Homepage.tsx
+++ web/src/pages/Homepage.tsx
@@ -1,15 +1,20 @@
 import "./HomePage.css";
 import { Map } from "../components/Map";
+import { useUser } from "@clerk/clerk-react";
 
 export function HomePage() {
+  const { user } = useUser();
   return (
     <>
       <h3>
+        {user && "Welcome " + `${user.username!}` + "!"} <br />
         There are X boxes near you, full of items looking for a new home!
         {/* Replace X with number of boxes visible on map or within defaul search parameters */}
         <br />
         Click on a box to see what's in it! <br /> If you take something from a
-        box, or notice something is already gone, please check it off the list!
+        box, or notice something is already gone, please check it off the list!{" "}
+        <br />
+        {!user && "Sign in or sign up to get started!"}
       </h3>
       <Map />
       <div className="start">
