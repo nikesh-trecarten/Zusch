f1e07df1075005ae5ddc0d3d32b899a2660bec69
Ralf Siewert <ralf@actyvy.st>
2025-04-28 16:07:19 +0200

    style input

--- app/app/sign-in.tsx
+++ app/app/sign-in.tsx
@@ -1,12 +1,12 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Pressable, Text, TextInput } from "react-native";
+import { Pressable, Text, TextInput, StyleSheet } from "react-native";
 import { globalStyles } from "@/styles/globalStyles";
 
 export default function SignInPage() {
   return (
-    <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <TextInput />
-      <TextInput />
+    <SafeAreaView style={[styles.container]}>
+      <TextInput style={[styles.input]} />
+      <TextInput style={[styles.input]} />
       <Pressable
         style={({ pressed }) => {
           return [globalStyles.button, pressed && globalStyles.buttonPressed];
@@ -18,3 +18,19 @@ export default function SignInPage() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    borderRadius: 5,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+  },
+});
