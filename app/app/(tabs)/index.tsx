160452e4424ddb34da20f4896c80efb1f66c59bb
Ralf Siewert <ralf@actyvy.st>
2025-04-29 10:47:49 +0200

    add axios

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,8 +1,57 @@
 import { Text, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { globalStyles } from "@/styles/globalStyles";
+import { useEffect, useCallback } from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import axios from "axios";
+import { useFocusEffect } from "expo-router";
+
+const API_HOST = process.env.EXPO_PUBLIC_API_HOST;
 
 export default function HomeScreen() {
+  const { getToken } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const token = await getToken();
+          const response = await axios.get(`${API_HOST}/boxes`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          console.log(response.data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+
+      fetchData();
+      console.log("I've got focus");
+      return () => {
+        console.log("I've lost focus");
+      };
+    }, [])
+  );
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const token = await getToken();
+  //       const response = await axios.get(`${API_HOST}/boxes`, {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
   return (
     <>
       <SafeAreaView style={[globalStyles.pageContainer]}>
